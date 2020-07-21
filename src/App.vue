d9059a160bcecaa7a4ac1a22f7b818bd5334ee0a
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2020-07-22 03:44:44 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <app-servers></app-servers>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style></style>
---
+++ src/ServerStatus.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+    <p>Server status {{ status }}</p>
+    <hr>
+    <button @click="changeStatus">Change</button>
+    </div>
+
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      status: "criticle"
+    };
+  },
+  methods:{
+    changeStatus(){
+      this.status='normal'
+    }
+  }
+};
+</script>
